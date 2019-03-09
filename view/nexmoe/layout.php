f6a5370e965788426726e79010b662c060958b09
muzhi621 <muzhi621@qq.com>
2019-03-09 12:12:48 +0800

    修改自用oneIndex

---
+++ view/nexmoe/layout.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
+	<title>muzhi</title>
+	<link rel="stylesheet" href="//cdnjs.loli.net/ajax/libs/mdui/0.4.1/css/mdui.css">
+   
+	
+  
+  
+  
+  <style>
+		body{
+          background-color:#f2f5fa;
+          
+          padding-bottom:60px;
+          background-position:center bottom;
+          background-repeat:no-repeat;
+          background-attachment:fixed
+            
+           
+      }
+       
+      
+      .nexmoe-item{
+        
+          
+        margin: 20px -8px 0 !important;
+		padding: 15px !important;
+		border-radius: 5px;
+		background-color: rgba(255, 255, 255, .6);
+       
+		-webkit-box-shadow: 0 .5em 3em rgba(161, 177, 204, .4);
+		box-shadow: 0 .5em 3em rgba(161, 177, 204, .4);
+		background-color: rgba(255, 255, 255, .6) 
+          
+      }
+      
+      .mdui-shadow-3 {
+			margin: 20px -8px 0 !important;
+			padding: 15px !important;
+			border-radius: 5px;
+			background-color: rgba(255, 255, 255, .6);
+			-webkit-box-shadow: 0 .5em 3em rgba(161, 177, 204, .4);
+			box-shadow: 0 .5em 3em rgba(161, 177, 204, .4);
+			background-color: rgba(255, 255, 255, .6)
+		}
+
+      
+      
+      .mdui-img-fluid,
+      .mdui-video-fluid
+      {
+        border-radius:5px;
+        border:1px solid #eee
+      }
+      
+      .mdui-list{
+        padding:0
+      }
+      .mdui-list-item
+      {
+        margin:0!important;
+        border-radius:5px;
+        padding:0 10px 0 5px!important;
+        border:1px solid #eee;
+        margin-bottom:10px!important
+      }
+      .mdui-list-item:last-child{
+        margin-bottom:0!important
+      }
+      .mdui-list-item:first-child
+      {
+        border:none
+      }
+      .mdui-toolbar
+      {
+        width:auto;
+        margin-top:60px!important
+      }
+      .mdui-appbar .mdui-toolbar{
+        height:56px;
+        font-size:16px
+      }
+      .mdui-toolbar>*{
+        padding:0 6px;
+        margin:0 2px;
+        opacity:.5
+      }
+      .mdui-toolbar>.mdui-typo-headline{
+        padding:0 16px 0 0
+      }
+      .mdui-toolbar>i{
+        padding:0
+      }
+      .mdui-toolbar>a:hover,
+      a.mdui-typo-headline,
+      a.active{
+        opacity:1
+      }
+      
+      
+      .mdui-typo-display-2-opacity
+		{
+    font-size: 45px;
+    font-weight: 400;
+    letter-spacing: 0;
+    color: rgb(255, 255, 255);
+	opacity: 1;
+    }
+      
+     .mdui-textfield.mdui-textfield-floating-label .mdui-icon.material-icons {
+			color: rgb(255, 255, 255);
+}
+.mdui-textfield.mdui-textfield-floating-label .mdui-textfield-label {
+			color: rgb(255, 255, 255);
+}
+			
+		
+		.mdui-textfield.mdui-textfield-floating-label .mdui-textfield-input {
+			color: rgb(255, 255, 255);
+  border-bottom: 1px solid rgba(255, 255, 255, .68);
+  border-bottom-color: #ffffff
+  border-radius: 0;
+  box-shadow: 0 1px 0 0 #ffffff;
+		} 
+      
+      
+      
+      
+      .mdui-container{
+        max-width:980px
+      }
+      .mdui-list>.th{
+        background-color:initial
+      }
+      .mdui-list-item>a{
+        width:100%;
+        line-height:48px
+      }
+      .mdui-toolbar>a{
+        padding:0 16px;
+        line-height:30px;
+        border-radius:30px;
+        border:1px solid #eee
+      }
+      .mdui-toolbar>a:last-child{
+        opacity:1;
+        background-color:#1e89f2;
+        color:#ffff
+      }
+      @media screen and (max-width:980px){
+        
+        .mdui-list-item .mdui-text-right
+        {
+          display:none
+        }
+        .mdui-container
+        {
+          width:100%!important;
+          margin:0
+        }
+        .mdui-toolbar>*{
+          display:none
+        }
+        .mdui-toolbar>a:last-child,
+        .mdui-toolbar>.mdui-typo-headline,
+        .mdui-toolbar>i:first-child{
+          display:block
+        }
+      }
+	</style>
+	<script src="//cdnjs.loli.net/ajax/libs/mdui/0.4.1/js/mdui.min.js"></script>
+
+  
+  
+  
+  
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mdui@0.4.1/dist/css/mdui.min.css" integrity="sha256-lCFxSSYsY5OMx6y8gp8/j6NVngvBh3ulMtrf4SX5Z5A=" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/mdui@0.4.1/dist/js/mdui.min.js" integrity="sha256-dZxrLDxoyEQADIAGrWhPtWqjDFvZZBigzArprSzkKgI=" crossorigin="anonymous"></script>
+<style>
+      body{
+             background: url(http://api.btstu.cn/sjbz/zsy.php)
+			
+  
+         }
+
+	</style>   
+  
+  
+
+  
+  
+  </head>
+<body class="mdui-theme-primary-blue-grey mdui-theme-accent-blue">
+	
+	<div class="mdui-container">
+	    <div class="mdui-container-fluid">
+          
+          
+          <style>
+.thumb .th{
+	display: none;
+}
+.thumb .mdui-text-right{
+	display: none;
+}
+.thumb .mdui-list-item a ,.thumb .mdui-list-item {
+	width:217px;
+	height: 230px;
+	float: left;
+	margin: 10px 10px !important;
+}
+
+.thumb .mdui-col-xs-12,.thumb .mdui-col-sm-7{
+	width:100% !important;
+	height:230px;
+}
+
+.thumb .mdui-list-item .mdui-icon{
+	font-size:100px;
+	display: block;
+	margin-top: 40px;
+	color: #7ab5ef;
+}
+.thumb .mdui-list-item span{
+	float: left;
+	display: block;
+	text-align: center;
+	width:100%;
+	position: absolute;
+    top: 180px;
+}
+</style>
+          
+          
+          
+	    <div class="mdui-toolbar nexmoe-item">
+			<a href="/"><?php e(config('site_name'));?></a>
+			<?php foreach((array)$navs as $n=>$l):?>
+			<i class="mdui-icon material-icons mdui-icon-dark" style="margin:0;">chevron_right</i>
+			<a href="<?php e($l);?>"><?php e($n);?></a>
+			<?php endforeach;?>
+			<!--<a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">refresh</i></a>-->
+		</div>
+		</div>
+    	<?php view::section('content');?>
+
+      
+  	</div>
+  
+</body>
+</html>
